b459e5dcfe4a9dee3209ffe5c027155c07e6143d
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2024-01-23 18:47:15 +0300

    *

--- pages/index.js
+++ pages/index.js
@@ -167,7 +167,7 @@ export default function Home() {
                 <div className="p-2 w-full">
                   <button
                     type="submit"
-                    className="flex mx-auto text-white bg-gradient-to-t to-purple-400 from-blue-400 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg"
+                    className="flex mx-auto text-white bg-gradient-to-t to-purple-400 from-blue-400 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg transition duration-300 ease-in-out"
                   >
                     Button
                   </button>
