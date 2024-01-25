5f19ef9f96dc0be870d4e6bc96234928b9fa3520
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2024-01-26 00:35:19 +0300

    form

--- pages/index.js
+++ pages/index.js
@@ -93,89 +93,113 @@ export default function Home() {
           name: "",
           email: "",
           message: "",
+          selectedOption: "",
         }}
         onSubmit={(values, { resetForm }) => {
           console.log(values);
-
-          // Form işlemleri tamamlandıktan sonra formu temizle
           resetForm();
         }}
       >
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-              Connect With Innovators
-            </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Get in touch with us to discover how our cutting-edge solutions
-              can revolutionize your digital experience and drive your success
-              in the ever-evolving tech landscape.
-            </p>
-          </div>
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-col text-center w-full mb-12">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+                  Connect With Innovators
+                </h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                  Get in touch with us to discover how our cutting-edge
+                  solutions can revolutionize your digital experience and drive
+                  your success in the ever-evolving tech landscape.
+                </p>
+              </div>
 
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <Form>
-              <div className="flex flex-wrap -m-2">
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="name"
-                      className="leading-7 text-sm text-gray-400"
-                    >
-                      Name
-                    </label>
-                    <Field
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
+              <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <div className="flex flex-wrap -m-2">
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label
+                        htmlFor="name"
+                        className="leading-7 text-sm text-gray-400"
+                      >
+                        Name
+                      </label>
+                      <Field
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="email"
-                      className="leading-7 text-sm text-gray-400"
-                    >
-                      Email
-                    </label>
-                    <Field
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label
+                        htmlFor="email"
+                        className="leading-7 text-sm text-gray-400"
+                      >
+                        Email
+                      </label>
+                      <Field
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-full">
-                  <div className="relative">
-                    <label
-                      htmlFor="message"
-                      className="leading-7 text-sm text-gray-400"
-                    >
-                      Message
-                    </label>
-                    <Field
-                      id="message"
-                      name="message"
-                      className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                      defaultValue={""}
-                    />
+                  <div className="p-2 w-full">
+                    <div className="relative">
+                      <label
+                        htmlFor="selectedOption"
+                        className="leading-7 text-sm text-gray-400"
+                      >
+                        Select an option:
+                      </label>
+                      <Field
+                        as="select"
+                        id="selectedOption"
+                        name="selectedOption"
+                        className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      >
+                        <option value="" label="Select an option" />
+                        {options.map((option) => (
+                          <option key={option.value} value={option.value}>
+                            {option.label}
+                          </option>
+                        ))}
+                      </Field>
+                    </div>
                   </div>
-                </div>
-                <div className="p-2 w-full">
-                  <button
-                    type="submit"
-                    className="flex mx-auto text-white bg-gradient-to-t to-purple-400 from-blue-400 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg transition duration-300 ease-in-out"
-                  >
-                    Button
-                  </button>
+                  <div className="p-2 w-full">
+                    <div className="relative">
+                      <label
+                        htmlFor="message"
+                        className="leading-7 text-sm text-gray-400"
+                      >
+                        Message
+                      </label>
+                      <Field
+                        id="message"
+                        name="message"
+                        className="w-full bg-gray-800 bg-opacity-40 rounded border border-blue-400 focus:border-purple-500 focus:bg-gray-900 focus:ring-2 focus:ring-purple-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        as="textarea"
+                      />
+                    </div>
+                  </div>
+                  <div className="p-2 w-full">
+                    <button
+                      type="submit"
+                      className="flex mx-auto text-white bg-gradient-to-t to-purple-400 from-blue-400 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg transition duration-300 ease-in-out"
+                    >
+                      Button
+                    </button>
+                  </div>
                 </div>
               </div>
-            </Form>
-          </div>
-        </div>
+            </div>
+          </Form>
+        )}
       </Formik>
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-24 mx-auto">
